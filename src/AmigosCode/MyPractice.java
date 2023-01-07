769ba968b79463c113b701fdeefe7a5f288325e4
chamalee samarawijaya <48786870+chapzchnz@users.noreply.github.com>
2023-01-08 01:19:31 +0200

    Initial commit

---
+++ src/AmigosCode/MyPractice.java
@@ -0,0 +1,12 @@
+package AmigosCode;
+
+import java.util.Locale;
+
+public class MyPractice {
+    public static void main(String[] args){
+        // non  primitive data types references
+        String name = new String("Chamalee");
+        System.out.println(name.toUpperCase(Locale.ROOT));
+
+    }
+}
---
+++ src/MyFirst.java
@@ -0,0 +1,23 @@
+import java.util.Date;
+
+public class MyFirst {
+
+    public static void main(String[] args){
+        int    sum;
+        double sum1;
+        float  sum2;
+        Date date = new Date();
+        byte theByte = -128;
+
+        sum= 50/6;
+        sum1=50/6d;
+        sum2=50/6f;
+
+        System.out.println(sum);
+        System.out.println(sum1);
+        System.out.println(sum2);
+        System.out.println(theByte);
+    }
+}
+
+
