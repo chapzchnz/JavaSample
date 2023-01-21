d60adf4888c609d68aa91fb69cabc2236efb068a
chamalee samarawijaya <48786870+chapzchnz@users.noreply.github.com>
2023-01-21 23:05:25 +0200

    Array examples

---
+++ src/AmigosCode/ArrayExample.java
@@ -0,0 +1,24 @@
+package AmigosCode;
+
+import java.util.Arrays;
+
+public class ArrayExample {
+    public static void main(String[] args) {
+        /**
+        int [] numbers = {0 ,1,2,3,4 } ;
+        String [] names = {"utu", "Titi","Mixi"};
+        System.out.println(Arrays.toString(numbers));
+        System.out.println(numbers.length);
+        System.out.println(Arrays.toString(names));
+        System.out.println(names.length);
+         **/
+     // array  and indexes
+        int [] numbers = {0, 1, 4, 5 , 100};
+                int hundred = numbers[4];
+                int four = numbers[2];
+        System.out.println(hundred);
+        System.out.println(four);
+
+    }
+
+}
