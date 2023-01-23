bc65d9db03a94d6ac38881498cf11abe0f5b4ebc
chamalee samarawijaya <48786870+chapzchnz@users.noreply.github.com>
2023-01-24 00:50:37 +0200

    While and DO while loops

--- src/AmigosCode/ArrayExample.java
+++ src/AmigosCode/ArrayExample.java
@@ -13,11 +13,55 @@ public class ArrayExample {
         System.out.println(names.length);
          **/
      // array  and indexes
-        int [] numbers = {0, 1, 4, 5 , 100};
+       /** int [] numbers = {0, 1, 4, 5 , 100};
                 int hundred = numbers[4];
                 int four = numbers[2];
         System.out.println(hundred);
         System.out.println(four);
+        **/
+    // increment and decrement
+      /**  int [] numbers = {0,1,2,3,4,5};
+        int number = 5;
+        number--;
+        System.out.println(number);
+       **/
+      // enhanced for loop
+        /**int [] numbers = {1,0,4, 7, 88, 99};
+                for( int number: numbers){
+                    System.out.println(number);
+                } **/
+       /** int [] numbers = {1,0,4,7,88,99};
+        for (int i=0; i < numbers.length; i++){
+            System.out.println(numbers[i]);
+        }    **/
+
+       /**String []names = {"Cherry","Cerry","Serry","Derry","Kirry"};
+               for(String name: names){
+                   if (name.equals("Serry")){
+                      continue;
+
+                   }
+                   System.out.println(name);
+               }   **/
+       //While loop
+       /** int count = 0;
+        while (count<=10){
+            System.out.println(count);
+            count++;
+        }**/
+       //Do while loop
+        int count = 21;
+        do {
+            System.out.println(count);
+            count++;
+        }
+         while (count<=20);
+
+            while (true){
+
+            }
+
+
 
     }
 
